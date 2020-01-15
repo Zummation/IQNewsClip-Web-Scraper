0b87a9beb318faa9ef7bf049e7e70d4bd4b1b911
tongplw <bangkok.tong@gmail.com>
2020-01-16 00:14:57 +0700

    update SET100 list

--- main_thread.py
+++ main_thread.py
@@ -1,11 +1,11 @@
 from IQNewsClipThread import IQNewsClipThread as Scraper
 
 
-keys = ['zn', 'zo', 'zx', 'zy', 'zz']
+# keys = ['zn', 'zo', 'zx', 'zy', 'zz']
 sources = ['ข่าวหุ้น', 'ทันหุ้น']
 
 with open('SET100.csv', 'r') as f:
-    keys = [symbol.strip() for symbol in f.readlines() if symbol > 'WH']
+    keys = [symbol.strip() for symbol in f.readlines()]
 
 
 scraper = Scraper(keys, sources, 3)
