23864937efe717cf60b079caaf853bbdfcade8b6
tongplw <bangkok.tong@gmail.com>
2020-01-17 18:43:40 +0700

    completed

--- mainWithThread.py
+++ mainWithThread.py
@@ -6,10 +6,8 @@ sources = ['ข่าวหุ้น', 'ทันหุ้น']
 with open('SET100.csv', 'r') as f:
     keys = [symbol.strip() for symbol in f.readlines()]
 
-print(keys)
-
-scraper = Scraper(keys, sources, n_thread=6)
-scraper.start()
-# scraper.create_newscount_file()
+scraper = Scraper(keys, sources, n_thread=3)
+# scraper.start()
+scraper.create_newscount_file()
 
 # TODO: Implement multi-processing and re-run
