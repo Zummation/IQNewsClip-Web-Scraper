1103d2e4b1ceb25d87aef15d7decd21624f8746c
tongplw <bangkok.tong@gmail.com>
2020-01-18 09:58:44 +0700

    create test function

--- test.py
+++ test.py
@@ -6,10 +6,17 @@ sources = ['ข่าวหุ้น', 'ทันหุ้น']
 with open('SET100.csv', 'r') as f:
     keys = [symbol.strip() for symbol in f.readlines()]
 
-for key in keys:
-    for source in sources:
-        df = pd.read_csv(f'result/{key}-{source}.csv')
-        if True in set(pd.isnull(df['HeadLine'])):
-            print('is null', key, source)
-        if len(df.index) == 0:
-            print('0 row', key, source)
+
+def test_charset_is_correct(keys, sources):
+    for key in keys:
+        for source in sources:
+            df = pd.read_csv(f'result/{key}-{source}.csv')
+            if True in set(pd.isnull(df['HeadLine'])):
+                print('is null', key, source)
+        
+def find_zero_row_page(keys, source):        
+    for key in keys:
+        for source in sources:
+            df = pd.read_csv(f'result/{key}-{source}.csv')
+            if len(df.index) == 0:
+                print('0 row', key, source)
