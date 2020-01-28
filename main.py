a3541ca899e3f0a5b01736ce39f2809f924a2c3b
tongplw <bangkok.tong@gmail.com>
2020-01-28 16:19:06 +0700

    fixes some bug

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     with open('SET100.csv', 'r') as f:
         keys = [symbol.strip() for symbol in f.readlines()]
 
-    scraper = Scraper(keys, sources, n_thread=10)
+    scraper = Scraper(keys, sources, n_thread=2)
     scraper.start()
     scraper.create_newscount_file()
 
